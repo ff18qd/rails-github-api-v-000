28a74b434386cc1d72eef42910a74c029a121c1e
EC2 Default User <ec2-user@ip-172-31-35-66.us-east-2.compute.internal>
2018-03-13 02:29:34 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,12 +2,19 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :authenticate_user
 
   private
 
     def authenticate_user
       # make sure to pass in the scope parameter (`repo` scope should be appropriate for what we want to do) in step of the auth process!
       # https://developer.github.com/apps/building-oauth-apps/authorization-options-for-oauth-apps/#web-application-flow
+      client_id = ENV['GITHUB_CLIENT_ID']
+      scopestring = 'repo'
+      redirect_uri = CGI.escape("https://80887eaf7a0b474e9fa06d513302b5b6.vfs.cloud9.us-east-2.amazonaws.com/auth")
+      github_url = "https://github.com/login/oauth/authorize?client_id=#{client_id}&scope=#{scopestring}&redirect_uri=#{redirect_uri}"
+      redirect_to github_url unless logged_in?
+      # redirect_to root_path 
     end
 
     def logged_in?
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,21 @@
 class SessionsController < ApplicationController
+  skip_before_action :authenticate_user
   def create
+    resp = Faraday.get("https://github.com/login/oauth/access_token") do |req|
+      req.params['client_id'] = ENV['GITHUB_CLIENT_ID']
+      req.params['client_secret'] = ENV['GITHUB_SECRET']
+      req.params['grant_type'] = 'authorization_code'
+      req.params['redirect_uri'] = "https://80887eaf7a0b474e9fa06d513302b5b6.vfs.cloud9.us-east-2.amazonaws.com/auth"
+      req.params['code'] = params[:code]
+    end
+   
+    body = JSON.parse(resp.body)
+    session[:token] = body["access_token"]
+    redirect_to root_path
+  end
+  
+  private 
+  def logged_in?
+    !!session[:token]
   end
 end
